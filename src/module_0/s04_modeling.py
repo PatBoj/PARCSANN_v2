91f8436dda7d5d9dca46f3904821ed486c1df1c4
Patryk <pbojarski94@gmail.com>
2023-06-01 21:55:30 +0200

    reafactor modeling

--- src/module_0/s04_modeling.py
+++ src/module_0/s04_modeling.py
@@ -4,7 +4,9 @@ import os
 
 from keras.models import Sequential
 from keras.layers import Dense, Normalization
+from keras import optimizers
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -14,7 +16,7 @@ def prepare_data(X: pd.DataFrame, y: pd.DataFrame, cfg: dict) -> tuple:
     
     X_train, X_test, y_train, y_test = train_test_split(
         X, y,
-        test_size=cfg.get('test_size'))
+        test_size=cfg.get('train_split'))
     
     return X_train, X_test, y_train, y_test
 
@@ -35,9 +37,9 @@ def add_layer(
         activation=activation, 
         input_dim=input_size))
 
+
 def create_nn(
-    cfg: dict, 
-    normalize: bool, 
+    cfg: dict,
     input_size: np.int, 
     output_size: np.int) -> Sequential:
     """ 
@@ -47,10 +49,10 @@ def create_nn(
     
     model = Sequential()
     
-    if normalize:
+    if cfg.get('normalize'):
         model.add(Normalization())
     
-    for layer in cfg.values():
+    for layer in cfg.get('layers').values():
         add_layer(
             model=model, 
             neurons=layer.get('neurons'), 
@@ -62,53 +64,61 @@ def create_nn(
 
 def compile_nn(
     model: Sequential, 
-    loss: str, 
-    optimizer: str):
+    cfg: dict):
     """ Compile neural network with given loss function and optimizer """
+
+    custom_optimizer = optimizers.Adam(learning_rate=cfg.get('learning_rate'))
     
-    model.compile(loss=loss, optimizer=optimizer)
+    model.compile(
+        loss=cfg.get('loss_function'), 
+        optimizer=custom_optimizer)
 
 
 def train_nn(
-    model: Sequential, 
-    X_train: np.ndarray, 
-    y_train: np.ndarray, 
-    epochs: np.int):
+    model: Sequential,
+    cfg: dict,
+    X_train: pd.DataFrame, 
+    X_test: pd.DataFrame,
+    y_train: pd.DataFrame,
+    y_test: pd.DataFrame):
     """ Train neural network with given data and epochs """
-    
-    model.fit(X_train, y_train, epochs=epochs)
 
-    
-def create_train_nn(X_train, y_train, cfg: dict) -> Sequential:
-    """ 
-    Create and train neural network based on the config file and given data
-    """
-    
-    model = create_nn(
-        cfg=cfg.get('layers'),
-        normalize=cfg.get('normalize'),
-        input_size=X_train.shape[1], 
-        output_size=y_train.shape[1])
-    
-    compile_nn(
-        model=model, 
-        loss=cfg.get('loss_function'), 
-        optimizer=cfg.get('optimizer'))
-    
-    train_nn(
-        model=model,
-        X_train=X_train,
-        y_train=y_train,
-        epochs=cfg.get('epochs'))
-    
-    return model
+    model.fit(
+        X_train, y_train, 
+        epochs=cfg.get('epochs'),
+        validation_data=(X_test, y_test))
+
+
+def plot_result(history):
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
 
 
 def get_test_predict(X: np.ndarray, y: np.ndarray, cfg: dict) -> tuple:
     """ Get prediction of the random sample along with testing data """
     
     X_train, X_test, y_train, y_test = prepare_data(X, y, cfg.get('data'))
-
-    model = create_train_nn(X_train, y_train, cfg.get('neural_network'))
+    
+    model = create_nn(
+        cfg=cfg.get('neural_network_layout'),
+        input_size=X_train.shape[1], 
+        output_size=y_train.shape[1])
+    
+    compile_nn(model=model, cfg=cfg.get('neural_network_compile'))
+    
+    train_nn(
+        model=model, 
+        cfg=cfg.get('neural_network_learning'),
+        X_train=X_train,
+        X_test=X_test,
+        y_train=y_train,
+        y_test=y_test)
+    
+    plot_result(model.history)
     
     return y_test, model.predict(X_test)
