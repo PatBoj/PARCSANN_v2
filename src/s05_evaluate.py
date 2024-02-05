ba5533425a76be27f8d7062acc2491d0ac953060
Patryk <pbojarski94@gmail.com>
2024-02-05 22:36:21 +0100

    adjust evaluate function

--- src/s05_evaluate.py
+++ src/s05_evaluate.py
@@ -1,37 +1,42 @@
+import sys
 import numpy as np
 import pandas as pd
 
 from utils.config import CFG
 from loguru import logger
 
-from utils.metrics import mean_absolute_error
-from utils.metrics import std_absolute_error
-from utils.metrics import mean_relative_error
-from utils.metrics import std_relative_error
 
-### FIXME: Refactor metric function
-def apply_single_metric(y_true: np.ndarray, y_pred: np.ndarray, metric: str) -> np.ndarray:
+@logger.catch(onerror=lambda _: sys.exit(1))
+def apply_single_metric(y_true: pd.DataFrame, y_pred: pd.DataFrame, metric: str) -> pd.DataFrame:
+    """ Calculate single metric """
     
-    if metric == 'mean_absolute_error':
-        return mean_absolute_error(y_true, y_pred)
-    elif metric == 'std_absolute_error':
-        return std_absolute_error(y_true, y_pred)
-    elif metric == 'mean_relative_error':
-        return mean_relative_error(y_true, y_pred)
-    elif metric == 'std_relative_error':
-        return std_relative_error(y_true, y_pred)
+    match metric:
+        case 'mean_absolute_error':
+            return (y_pred - y_true).abs().mean()
+        
+        case 'std_absolute_error':
+            return (y_pred - y_true).abs().std()
+        
+        case 'mean_absolute_percentage_error':
+            return (1 - (y_pred / y_true)).abs().mean()
+        
+        case 'std_absolute_percentage_error':
+            return (1 - (y_pred / y_true)).abs().std()
+        
+        case _:
+            logger.error(f'Metric "{metric}" does not exist.')
 
 
-def evaluate_model(y_true: np.ndarray, y_pred: np.ndarray, output_column_names: list) -> pd.DataFrame:
-
-    metric_matix = np.empty((0, len(output_column_names)))
+def evaluate_model(y_true: pd.DataFrame, y_pred: pd.DataFrame) -> pd.DataFrame:
+    """ Calculate all metrics for output data """
+    
+    evaluation_df = pd.DataFrame()
     
     for metric in CFG['metrics']:
         logger.info(f'Calculating {metric}.')
-        metric_row = apply_single_metric(y_true, y_pred, metric)
-        metric_matix = np.append(metric_matix, [metric_row], axis=0)
+        evaluation_single = apply_single_metric(y_true, y_pred, metric)
+        evaluation_df = pd.concat([evaluation_df, evaluation_single], axis=1)
     
-    logger.info('Creating metric dataframe.')
-    metric_df = pd.DataFrame(metric_matix, columns=output_column_names, index=CFG['metrics'])
+    evaluation_df.columns = CFG['metrics']
     
-    return metric_df
+    return evaluation_df
